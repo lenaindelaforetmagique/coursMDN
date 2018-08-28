96ce626f635fdcc12289b4ae65f97ab88054242c
lenaindelaforetmagique <X.MORIN@bycn.bouygues-construction.com>
2018-08-28 15:16:01 +0200

    ajout test

--- JS/script/script.js
+++ JS/script/script.js
@@ -10,20 +10,42 @@ if (parfumGlace === 'Chocolat') {
 }
 /**/
 
-var objet = document.querySelector('img.logo');
+var logo = document.querySelector('img.logo');
 var cptClic = 0;
-objet.onclick = function() {
-
+logo.onclick = function() {
   cptClic += 1;
   document.querySelector('p.compteur').textContent = 'Vous avez cliqué ' + cptClic + ' fois !';
-  var img = document.querySelector('img.logo')
-  var src = img.getAttribute('src');
+
+  var src = logo.getAttribute('src');
+  console.log(src);
   src = 'images/firefox-icon-' + cptClic % 4 + '.png'
 
-  img.setAttribute('src', src);
-
-
+  logo.setAttribute('src', src);
 }
+
+
+var button = document.querySelector('button');
+var monTitre = document.querySelector('h1');
+
+function defineUser() {
+  var myName = prompt('Veuillez saisir votre nom.');
+  localStorage.setItem('nom', myName);
+  monTitre.textContent = 'Mozilla est cool, ' + myName;
+}
+
+
+if (!localStorage.getItem('nom')) {
+  defineUser();
+} else {
+
+  var myName = localStorage.getItem('nom');
+  monTitre.textContent = 'Mozilla est cool, ' + myName;
+}
+
+button.onclick = function() {
+  defineUser();
+}
+
 
 
 /* bloc de commentaires */
